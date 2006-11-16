1a547518d5b5a387bd82b94ae2f8c74e4de1857d
Harri Pitkänen <hatapitk@iki.fi>
2006-11-16 15:09:07 +0000

    * Support parallel installations of Automake versions   1.9 and 1.10 in libvoikko and tmispell-voikko.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@774 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/autogen.sh
+++ libvoikko/autogen.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Automake version. Versions 1.9 and 1.10 are known to work.
+AM_VERSION_SUFFIX=-1.9
+
 echo Cleaning autotools files...
 find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
 find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
@@ -14,8 +17,8 @@ cp /usr/share/gettext/config.rpath .
 
 libtoolize --force --copy
 autoheader
-aclocal 
-automake-1.9 --add-missing --copy --force-missing --foreign
+aclocal$AM_VERSION_SUFFIX 
+automake$AM_VERSION_SUFFIX --add-missing --copy --force-missing --foreign
 autoconf
 autoheader
 #autoreconf
