69821f81cb7e93df84f2eeb26d476a2c56a71c9b
Hannu Väisänen <hannu.vaisanen@uef.fi>
2007-05-16 07:06:52 +0000

    Lisätty Python-ohjelmat Sukija-version sanaston generoimiseksi.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@1427 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- suomimalaga/voikko/common.py
+++ suomimalaga/voikko/common.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # Copyright 2007 Harri Pitkänen (hatapitk@iki.fi)
-# Program to generate lexicon files for Suomi-malaga Voikko edition
-
+#                Hannu Väisänen (Etunimi.Sukunimi@joensuu.fi)
+#
+# Functions and variables that are common to Sukija and Voikko versions.
+#
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
@@ -16,6 +18,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import hfconv
+import voikkoutils
+import codecs
 
 # Path to source data directory
 VOIKKO_DATA = u"vocabulary"
@@ -33,16 +39,6 @@ SPECIAL_VOCABULARY = [
 	('style', 'foreign', 'vieraskieliset.lex')]
 
 
-import hfconv
-import voikkoutils
-import sukija
-import xml.dom.minidom
-import codecs
-import sys
-import getopt
-
-flag_attributes = voikkoutils.readFlagAttributes(VOIKKO_DATA + u"/flags.txt")
-
 def open_lex(filename):
 	file = codecs.open(VOIKKO_LEX + u"/" + filename, 'w', 'UTF-8')
 	file.write(u"# This is automatically generated intermediate lexicon file for\n")
@@ -53,6 +49,7 @@ def open_lex(filename):
 	file.write(u"# generate this file (or instructions to obtain them) wherever\n")
 	file.write(u"# you got this file from.\n\n")
 	return file
+
 
 main_vocabulary = open_lex("joukahainen.lex")
 vocabulary_files = {}
@@ -72,10 +69,36 @@ def tValues(group, element_name):
 		values.append(tValue(element))
 	return values
 
-def frequency(word):
-	fclass = word.getElementsByTagName("fclass")
-	if len(fclass) == 0: return 7
-	return int(tValue(fclass[0]))
+# Returns malaga word class for given word in Joukahainen
+def get_malaga_word_class(j_wordclasses):
+	if "pnoun_place" in j_wordclasses: return u"paikannimi"
+	if "pnoun_firstname" in j_wordclasses: return u"etunimi"
+	if "pnoun_lastname" in j_wordclasses: return u"sukunimi"
+	if "pnoun_misc" in j_wordclasses: return u"nimi"
+	if "verb" in j_wordclasses: return u"teonsana"
+	if "adjective" in j_wordclasses and "noun" in j_wordclasses: return u"nimi_laatusana"
+	if "adjective" in j_wordclasses: return u"laatusana"
+	if "noun" in j_wordclasses: return u"nimisana"
+	return None
+
+# Returns malaga flags for given word in Joukahainen
+def get_malaga_flags(word):
+	global flag_attributes
+	malaga_flags = []
+	for flag_attribute in flag_attributes:
+		group = word.getElementsByTagName(flag_attribute.xmlGroup)
+		if len(group) == 0: continue
+		if flag_attribute.xmlFlag in tValues(group[0], "flag") and \
+		   flag_attribute.malagaFlag != None:
+			malaga_flags.append(flag_attribute.malagaFlag)
+	if len(malaga_flags) == 0: return u""
+	flag_string = u", tiedot: <"
+	for flag in malaga_flags:
+		flag_string = flag_string + flag + u","
+	flag_string = flag_string[:-1] + u">"
+	return flag_string
+
+flag_attributes = voikkoutils.readFlagAttributes(VOIKKO_DATA + u"/flags.txt")
 
 def vowel_type(group):
 	vtypes = group.getElementsByTagName("vtype")
@@ -119,34 +142,6 @@ def get_malaga_inflection_class(wordform, j_infclass, j_wordclasses):
 	
 	return (None, None)
 
-# Returns malaga word class for given word in Joukahainen
-def get_malaga_word_class(j_wordclasses):
-	if "pnoun_place" in j_wordclasses: return u"paikannimi"
-	if "pnoun_firstname" in j_wordclasses: return u"etunimi"
-	if "pnoun_lastname" in j_wordclasses: return u"sukunimi"
-	if "pnoun_misc" in j_wordclasses: return u"nimi"
-	if "verb" in j_wordclasses: return u"teonsana"
-	if "adjective" in j_wordclasses and "noun" in j_wordclasses: return u"nimi_laatusana"
-	if "adjective" in j_wordclasses: return u"laatusana"
-	if "noun" in j_wordclasses: return u"nimisana"
-	return None
-
-# Returns malaga flags for given word in Joukahainen
-def get_malaga_flags(word):
-	global flag_attributes
-	malaga_flags = []
-	for flag_attribute in flag_attributes:
-		group = word.getElementsByTagName(flag_attribute.xmlGroup)
-		if len(group) == 0: continue
-		if flag_attribute.xmlFlag in tValues(group[0], "flag") and \
-		   flag_attribute.malagaFlag != None:
-			malaga_flags.append(flag_attribute.malagaFlag)
-	if len(malaga_flags) == 0: return u""
-	flag_string = u", tiedot: <"
-	for flag in malaga_flags:
-		flag_string = flag_string + flag + u","
-	flag_string = flag_string[:-1] + u">"
-	return flag_string
 
 # Returns a string describing the structure of a word, if necessary for the spellchecker
 # or hyphenator
@@ -177,7 +172,7 @@ def get_structure(wordform, malaga_word_class):
 # Writes the vocabulary entry to a suitable file
 def write_entry(word, entry):
 	global vocabulary_files
-	global main_vocabulary
+        global main_vocabulary
 	special = False
 	for voc in SPECIAL_VOCABULARY:
 		group = word.getElementsByTagName(voc[0])
@@ -187,103 +182,3 @@ def write_entry(word, entry):
 			special = True
 	if not special:
 		main_vocabulary.write(entry + u"\n")
-
-def handle_word(word):
-	# Drop words that are not needed in the Voikko lexicon
-	if has_flag(word, "not_voikko"): return
-	if has_flag(word, "incorrect"): return
-	if has_flag(word, "dialect"): return
-	if frequency(word) >= 10: return
-	if frequency(word) == 9 and has_flag(word, "confusing"): return
-	
-	# Get the inflection class. Exactly one inflection class is needed
-	infclasses = word.getElementsByTagName("infclass")
-	voikko_infclass = None
-	for infclass in word.getElementsByTagName("infclass"):
-		if infclass.getAttribute("type") != "historical":
-			voikko_infclass = tValue(infclass)
-			break
-	if voikko_infclass == None: return
-	if voikko_infclass == u"poikkeava": return
-	
-	# Get the word classes
-	wordclasses = tValues(word.getElementsByTagName("classes")[0], "wclass")
-	malaga_word_class = get_malaga_word_class(wordclasses)
-	if malaga_word_class == None: return
-	
-	# Get malaga flags
-	malaga_flags = get_malaga_flags(word)
-	
-	# Get forced vowel type
-	forced_inflection_vtype = vowel_type(word.getElementsByTagName("inflection")[0])
-	
-	# Process all alternative forms
-	for altform in tValues(word.getElementsByTagName("forms")[0], "form"):
-		wordform = altform.replace(u'|', u'').replace(u'=', u'')
-		(alku, jatko) = get_malaga_inflection_class(wordform, voikko_infclass, wordclasses)
-		if forced_inflection_vtype == voikkoutils.VOWEL_DEFAULT:
-			vtype = voikkoutils.get_wordform_infl_vowel_type(altform)
-		else: vtype = forced_inflection_vtype
-		if vtype == voikkoutils.VOWEL_FRONT: malaga_vtype = u'ä'
-		elif vtype == voikkoutils.VOWEL_BACK: malaga_vtype = u'a'
-		elif vtype == voikkoutils.VOWEL_BOTH: malaga_vtype = u'aä'
-		rakenne = get_structure(altform, malaga_word_class)
-		if alku == None:
-			write_entry(word, u"#Malaga class not found for (%s, %s)\n" \
-			            % (wordform, voikko_infclass))
-			continue
-		entry = u'[perusmuoto: "%s", alku: "%s", luokka: %s, jatko: <%s>, äs: %s%s%s];' \
-		          % (wordform, alku, malaga_word_class, jatko, malaga_vtype, malaga_flags,
-				   get_structure(altform, malaga_word_class))
-		write_entry(word, entry)
-
-
-def usage():
-	print "Usage: generate_lex.py [-s]"
-	print "-s: generate Sukija version."
-	sys.exit()
-
-
-try:
-	opts, args = getopt.getopt(sys.argv[1:], "s", ["sukija"])
-except getopt.GetoptError:
-	usage()
-sukija_version = False
-for o, a in opts:
-	if o == "-s":
-		sukija_version = True
-
-
-listfile = open(VOIKKO_DATA + u'/joukahainen.xml', 'r')
-
-line = ""
-while line != '<wordlist xml:lang="fi">\n':
-	line = listfile.readline()
-	if line == '':
-		sys.stderr.write("Malformed file " + VOIKKO_DATA + "/joukahainen.xml\n")
-		sys.exit(1)
-
-wcount = 0
-while True:
-	wordstr = ""
-	line = listfile.readline()
-	if line == "</wordlist>\n": break
-	while line != '</word>\n':
-		wordstr = wordstr + line
-		line = listfile.readline()
-	word = xml.dom.minidom.parseString(wordstr + line)
-	if sukija_version:
-		sukija.handle_word(word)
-	else:
-		handle_word(word)
-	wcount = wcount + 1
-	if wcount % 1000 == 0:
-		sys.stdout.write("#")
-		sys.stdout.flush()
-
-sys.stdout.write("\n")
-listfile.close()
-main_vocabulary.close()
-for (name, file) in vocabulary_files.iteritems():
-	file.close()
-
---
+++ suomimalaga/voikko/sukija.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2007 Hannu Väisänen (Etunimi.Sukunimi@joensuu.fi
+# Program to generate lexicon files for Suomi-malaga Sukija edition.
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+# This code is heavily based on code written by Harri Pitkänen.
+
+import common
+import hfconv
+import voikkoutils
+
+
+def handle_word(word):
+	# Get the inflection class. Exactly one inflection class is needed
+	infclasses = word.getElementsByTagName("infclass")
+	voikko_infclass = None
+	for infclass in word.getElementsByTagName("infclass"):
+		if infclass.getAttribute("type") == "historical":
+			voikko_infclass = common.tValue(infclass)
+			break
+	if voikko_infclass == None:
+		for infclass in word.getElementsByTagName("infclass"):
+			if infclass.getAttribute("type") != "historical":
+				voikko_infclass = common.tValue(infclass)
+				break
+
+	if voikko_infclass == None: return
+	if voikko_infclass == u"poikkeava": return
+	
+	# Get the word classes
+	wordclasses = common.tValues(word.getElementsByTagName("classes")[0], "wclass")
+	malaga_word_class = common.get_malaga_word_class(wordclasses)
+	if malaga_word_class == None: return
+	
+	# Get malaga flags
+	malaga_flags = common.get_malaga_flags(word)
+	
+	# Get forced vowel type
+	forced_inflection_vtype = common.vowel_type(word.getElementsByTagName("inflection")[0])
+	
+	# Process all alternative forms
+	for altform in common.tValues(word.getElementsByTagName("forms")[0], "form"):
+		wordform = altform.replace(u'|', u'').replace(u'=', u'')
+		(alku, jatko) = common.get_malaga_inflection_class(wordform, voikko_infclass, wordclasses)
+		if forced_inflection_vtype == voikkoutils.VOWEL_DEFAULT:
+			vtype = voikkoutils.get_wordform_infl_vowel_type(altform)
+		else: vtype = forced_inflection_vtype
+		if vtype == voikkoutils.VOWEL_FRONT: malaga_vtype = u'ä'
+		elif vtype == voikkoutils.VOWEL_BACK: malaga_vtype = u'a'
+		elif vtype == voikkoutils.VOWEL_BOTH: malaga_vtype = u'aä'
+		rakenne = common.get_structure(altform, malaga_word_class)
+		if alku == None:
+			common.write_entry(word, u"#Malaga class not found for (%s, %s)\n" \
+			                   % (wordform, voikko_infclass))
+			continue
+		entry = u'[perusmuoto: "%s", alku: "%s", luokka: %s, jatko: <%s>, äs: %s%s%s];' \
+		          % (wordform, alku, malaga_word_class, jatko, malaga_vtype, malaga_flags,
+				   common.get_structure(altform, malaga_word_class))
+		common.write_entry(word, entry)
