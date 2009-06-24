a75d449487ff3a4f57373677d64e8d85763c11ec
Harri Pitkänen <hatapitk@iki.fi>
2009-06-24 16:16:25 +0000

    * Replace the commands used to generate autotools files   in autogen.sh with single call to autoreconf. Together with   using libtool 2 to generate the source package this should   fix SF.net bug #2810258
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@2730 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/autogen.sh
+++ libvoikko/autogen.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Automake version.
-AM_VERSION_SUFFIX=-1.10
-
 echo Cleaning autotools files...
 find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
 find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
@@ -16,11 +13,7 @@ cp /usr/share/misc/config.sub .
 cp /usr/share/misc/config.guess .
 cp /usr/share/gettext/config.rpath .
 
-libtoolize --force --copy
-autoheader
-aclocal$AM_VERSION_SUFFIX 
-automake$AM_VERSION_SUFFIX --add-missing --copy --force-missing --foreign
-autoconf
-autoheader
-#autoreconf
+echo Creating autotools files...
+mkdir -p m4
+autoreconf --force --install
 
