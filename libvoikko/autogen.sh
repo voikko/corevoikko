b1d9d41a235a892283f70510dce2f295c36947cb
Harri Pitkänen <hatapitk@iki.fi>
2009-10-04 13:36:39 +0000

    * Include libvoikko.vcproj in the distribution. * From Lauri Nurmi:   - Fix autogen.sh to work with non-GNU find and xargs.
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@2908 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/autogen.sh
+++ libvoikko/autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo Cleaning autotools files...
-find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
-find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
+find . -type d -name autom4te.cache | xargs rm -rf \;
+find . -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
         -o -name depcomp -o -name ltmain.sh -o -name configure \
         -o -name config.sub -o -name config.guess \
-        -o -name Makefile.in \) -print0 | xargs -0 rm -f
+        -o -name Makefile.in \) | xargs rm -f
 
 rm -f config.sub config.guess aclocal.m4 config.h.in config.rpath
 rm -rf m4
