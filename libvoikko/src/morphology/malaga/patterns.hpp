0afe64a5b03c62f24f27e0f570b672de3ca42883
Harri Pitkänen <hatapitk@iki.fi>
2009-10-23 12:47:44 +0000

    * Remove unused code
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@2968 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/src/morphology/malaga/patterns.hpp
+++ libvoikko/src/morphology/malaga/patterns.hpp
@@ -17,15 +17,8 @@ extern string_t pattern_var[ PATTERN_VAR_MAX ]; /* Pattern variables. FIXME */
 
 /* Functions. ===============================================================*/
 
-extern string_t compile_pattern( string_t string, int_t pattern_var_no );
-/* Convert STRING to a pattern to be used as input to "match_pattern".
- * If PATTERN_VAR_NO != -1, mark the pattern so the string matching this
- * pattern will be stored in PATTERN_VAR[ PATTERN_VAR_NO ].
- * The result pattern must be freed after usage. */
-
 extern bool match_pattern( string_t string, string_t pattern );
-/* Test whether STRING matches PATTERN (a string of chars compiled with
- * "compile_pattern") and set substring indices in PATTERN_VAR.
+/* Test whether STRING matches PATTERN and set substring indices in PATTERN_VAR.
  * The substrings remain valid until "compile_pattern" is called again. */
 
 extern void terminate_patterns( void );
