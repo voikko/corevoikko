0b929ef69ee43e7db926341465e8c7090b1e3b90
Harri Pitkänen <hatapitk@iki.fi>
2009-11-23 20:45:30 +0000

    * Add first draft of a general Hyphenator interface
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@3063 ee56993d-3414-0410-8ed4-8136e77a0dd8

---
+++ libvoikko/src/hyphenator/Hyphenator.hpp
@@ -0,0 +1,80 @@
+/* Libvoikko: Library of Finnish language tools
+ * Copyright (C) 2009 Harri Pitkänen <hatapitk@iki.fi>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *********************************************************************************/
+
+#ifndef VOIKKO_HYPHENATOR_HYPHENATOR
+#define VOIKKO_HYPHENATOR_HYPHENATOR
+
+#include <cstring>
+
+namespace libvoikko { namespace hyphenator {
+/**
+ * General interface for hyphenators.
+ */
+class Hyphenator {
+	public:
+		/**
+		 * Hyphenate given word.
+		 * @param word word to hyphenate
+		 * @param wlen length of the word in wchar_t units
+		 * @return null-terminated character string containing the hyphenation
+		 * using the following notation:
+		 *    ' ' = no hyphenation at this character
+		 *    '-' = hyphenation point (character at this position
+		 *          is preserved in the hyphenated form)
+		 *    '=' = hyphentation point (character at this position
+		 *          is replaced with hyphen.)
+		 * Returns null on error.
+		 */
+		virtual char * hyphenate(const wchar_t * word, size_t wlen) = 0;
+
+		/**
+		 * Terminate this component.
+		 */
+		virtual void terminate() = 0;
+
+		/**
+		 * Do not insert hyphenation positions that are considered to be ugly
+		 * but correct. Default: false
+		 */
+		virtual void setUglyHyphenation(bool uglyHyphenation) = 0;
+
+		/**
+		 * Hyphenate unknown words. Default: true
+		 */
+		virtual void setHyphenateUnknown(bool hyphenateUnknown) = 0;
+
+		/**
+		 * There are two possible rules that can be applied when hyphenating
+		 * compound words that can be split in more than one different way. We
+		 * either take the intersection of (1) all possible hyphenations or
+		 * (2) all hyphenations where the compound word has the minimal number
+		 * of parts (:= m) in it. The rule (1) is applied if and only if
+		 * m > voikko_intersect_compound_level. Default: 1
+		 */
+		virtual void setIntersectCompoundLevel(int level) = 0;
+
+		/**
+		 * The minumum length for words that may be hyphenated. This limit is
+		 * also enforced on individual parts of compound words. Default: 2
+		 */
+		virtual void setMinHyphenatedWordLength(int length) = 0;
+};
+
+} }
+
+#endif
