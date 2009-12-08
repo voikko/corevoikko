2bca40ce1a561f9c52ecaef9afccc7a1019b9590
Harri Pitkänen <hatapitk@iki.fi>
2009-12-08 21:01:36 +0000

    * Drop GPLv2 license from WWW tools
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@3073 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- tools/www/ajaxvoikko.py
+++ tools/www/ajaxvoikko.py
@@ -4,9 +4,9 @@
 # Web server that provides Ajax interface for using Voikko.
 # Requires Python version 2.5 or newer and Python interface to libvoikko.
 
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
@@ -15,8 +15,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from BaseHTTPServer import BaseHTTPRequestHandler
 from BaseHTTPServer import HTTPServer
