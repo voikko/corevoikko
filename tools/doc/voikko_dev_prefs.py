ca66ec093db3582e35b905a9b0f16781c48bff72
Harri Pitkänen <hatapitk@iki.fi>
2010-04-09 13:00:32 +0000

    * Update sample voikko_dev_prefs.py
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@3308 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- tools/doc/voikko_dev_prefs.py
+++ tools/doc/voikko_dev_prefs.py
@@ -13,8 +13,20 @@
 # The location of Voikko SVN repository checkout
 #svnroot='/home/uname/svn/voikko'
 
-# Data directory for voikkotest
+# Data directory for voikkotest. This directory must
+# already exist. If you want to test spell checker between
+# different revisions of the softwre this directory must
+# contain test data as gzipped list of words separated by
+# line feeds as 'wordlist.txt.gz'. Otherwise the directory
+# may be empty.
 #voikkotest_dir='/home/uname/tmp/voikkotest'
+
+# Number of threads to use when running voikkospell on
+# wordlist.txt.gz. The default is to use one thread.
+#voikkospell_threads=1
+
+# Language variant to use in the tests.
+#language_variant='standard'
 
 # Build options used when calling "make voikko"
 #voikkotest_build_options=''
@@ -24,14 +36,17 @@
 # tests. None means that installation is not performed.
 #voikkotest_sm_destdir=None
 
-# Data directory for voikko
+# Directory containing tests and other necessary data files.
 #voikko_data_dir='/home/uname/svn/voikko/trunk/data'
 
 # The input/output encoding for programs that read
 # or write data.
 #encoding='UTF-8'
 
-# The installation directory of libvoikko binaries
+# The installation directory of libvoikko binaries. Note that
+# this does not affect tests that are run through Python API.
+# To have them run using a library in a non-standard installation
+# directory you must set LD_LIBRARY_PATH appropriately.
 #libvoikko_bin='/usr/bin'
 
 # Command for viewing differences between 'base' and 'current'.
