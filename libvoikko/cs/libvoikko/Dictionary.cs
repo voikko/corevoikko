f5f509044e9e1853daef10d74a84ee00df059d10
Harri Pitkänen <hatapitk@iki.fi>
2011-04-27 15:28:49 +0000

    * more C# interface
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@3885 ee56993d-3414-0410-8ed4-8136e77a0dd8

---
+++ libvoikko/cs/libvoikko/Dictionary.cs
@@ -0,0 +1,85 @@
+using System;
+namespace libvoikko
+{
+	public class Dictionary : IComparable<Dictionary>
+	{
+
+		private readonly string language;
+		private string Language {
+			get { return language; }
+		}
+
+		private readonly string variant;
+		private string Variant {
+			get { return variant; }
+		}
+
+		private readonly string description;
+		private string Description {
+			get { return description; }
+		}
+
+		public Dictionary(String language, String variant, String description)
+		{
+			this.language = language;
+			this.variant = variant;
+			this.description = description;
+		}
+
+		public int CompareTo(Dictionary other)
+		{
+			int cmp = language.CompareTo(other.Language);
+			if (cmp != 0) {
+				return cmp;
+			}
+			cmp = variant.CompareTo(other.Variant);
+			if (cmp != 0) {
+				return cmp;
+			}
+			return description.CompareTo(other.Description);
+		}
+
+		public override int GetHashCode()
+		{
+			const int prime = 31;
+			int result = 1;
+			result = prime * result + ((description == null) ? 0 : description.GetHashCode());
+			result = prime * result + ((language == null) ? 0 : language.GetHashCode());
+			result = prime * result + ((variant == null) ? 0 : variant.GetHashCode());
+			return result;
+		}
+
+		public override bool Equals(Object obj)
+		{
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (!(obj is Dictionary))
+				return false;
+			Dictionary other = (Dictionary)obj;
+			if (description == null) {
+				if (other.description != null)
+					return false;
+			} else if (!description.Equals(other.Description))
+				return false;
+			if (language == null) {
+				if (other.language != null)
+					return false;
+			} else if (!language.Equals(other.Language))
+				return false;
+			if (variant == null) {
+				if (other.variant != null)
+					return false;
+			} else if (!variant.Equals(other.Variant))
+				return false;
+			return true;
+		}
+		
+		public override string ToString()
+		{
+			 return "<" + language + "," + variant + "," + description + ">";
+		}
+		
+	}
+}
--- libvoikko/cs/libvoikko/Test.cs
+++ libvoikko/cs/libvoikko/Test.cs
@@ -6,13 +6,13 @@ namespace libvoikko
 	public class Test
 	{
 		private Voikko voikko;
-		
+
 		[SetUp]
 		public void setUp()
 		{
 			voikko = new Voikko("fi");
 		}
-		
+
 		[TearDown]
 		public void tearDown()
 		{
@@ -23,29 +23,63 @@ namespace libvoikko
 			GC.Collect();
 			GC.WaitForPendingFinalizers();
 		}
-		
+
 		[Test]
 		public void initAndTerminate()
 		{
 			// do nothing, just check that setUp and tearDown complete successfully
 		}
-		
+
 		[Test]
 		public void terminateCanBeCalledMultipleTimes()
 		{
 			voikko.Dispose();
 			voikko.Dispose();
 		}
-		
+
 		[Test]
-		public void anotherObjectCanBeCreatedUsedAndDeletedInParallel() {
+		public void anotherObjectCanBeCreatedUsedAndDeletedInParallel()
+		{
 			Voikko medicalVoikko = new Voikko("fi-x-medicine");
 			Assert.IsTrue(medicalVoikko.Spell("amifostiini"));
 			Assert.IsFalse(voikko.Spell("amifostiini"));
 			medicalVoikko.Dispose();
 			Assert.IsFalse(voikko.Spell("amifostiini"));
 		}
-		
+
+		[Test]
+		public void dictionaryComparisonWorks()
+		{
+			Dictionary d1 = new Dictionary("fi", "a", "b");
+			Dictionary d2 = new Dictionary("fi", "a", "c");
+			Dictionary d3 = new Dictionary("fi", "c", "b");
+			Dictionary d4 = new Dictionary("fi", "a", "b");
+			Dictionary d5 = new Dictionary("sv", "a", "b");
+			Assert.IsFalse(d1.Equals("kissa"));
+			Assert.IsFalse("kissa".Equals(d1));
+			Assert.IsFalse(d1.Equals(d2));
+			Assert.IsFalse(d1.Equals(d3));
+			Assert.IsFalse(d4.Equals(d5));
+			Assert.IsTrue(d1.Equals(d4));
+			Assert.IsTrue(d1.CompareTo(d2) < 0);
+			Assert.IsTrue(d2.CompareTo(d3) < 0);
+			Assert.IsTrue(d4.CompareTo(d5) < 0);
+		}
+
+		[Test]
+		public void dictionaryHashCodeWorks()
+		{
+			Dictionary d1 = new Dictionary("fi", "a", "b");
+			Dictionary d2 = new Dictionary("fi", "a", "c");
+			Dictionary d3 = new Dictionary("fi", "c", "b");
+			Dictionary d4 = new Dictionary("fi", "a", "b");
+			Dictionary d5 = new Dictionary("sv", "a", "b");
+			Assert.AreNotEqual(d1.GetHashCode(), d2.GetHashCode());
+			Assert.AreNotEqual(d1.GetHashCode(), d3.GetHashCode());
+			Assert.AreNotEqual(d4.GetHashCode(), d5.GetHashCode());
+			Assert.AreEqual(d1.GetHashCode(), d4.GetHashCode());
+		}
+
 		[Test]
 		public void spell()
 		{
