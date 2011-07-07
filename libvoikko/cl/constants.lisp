b372fd8bf4b075e4aae576807470d85dedafd81c
Teemu Likonen <tlikonen@iki.fi>
2011-07-07 14:29:58 +0000

    Remove unnecessary comments from constants.lisp (voikko_defines.h)
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@3958 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/cl/constants.lisp
+++ libvoikko/cl/constants.lisp
@@ -16,30 +16,10 @@
 
 (in-package :voikko)
 
-;; VOIKKO_SPELL_FAILED 0
-;; VOIKKO_SPELL_OK 1
-;; VOIKKO_INTERNAL_ERROR 2
-;; VOIKKO_CHARSET_CONVERSION_FAILED 3
-
 (defconstant +voikko-spell-failed+ 0)
 (defconstant +voikko-spell-ok+ 1)
 (defconstant +voikko-internal-error+ 2)
 (defconstant +voikko-charset-conversion-failed+ 3)
-
-;; VOIKKO_OPT_IGNORE_DOT 0
-;; VOIKKO_OPT_IGNORE_NUMBERS 1
-;; VOIKKO_OPT_IGNORE_UPPERCASE 3
-;; VOIKKO_OPT_ACCEPT_FIRST_UPPERCASE 6
-;; VOIKKO_OPT_ACCEPT_ALL_UPPERCASE 7
-;; VOIKKO_OPT_NO_UGLY_HYPHENATION 4
-;; VOIKKO_OPT_OCR_SUGGESTIONS 8
-;; VOIKKO_OPT_IGNORE_NONWORDS 10
-;; VOIKKO_OPT_ACCEPT_EXTRA_HYPHENS 11
-;; VOIKKO_OPT_ACCEPT_MISSING_HYPHENS 12
-;; VOIKKO_OPT_ACCEPT_TITLES_IN_GC 13
-;; VOIKKO_OPT_ACCEPT_UNFINISHED_PARAGRAPHS_IN_GC 14
-;; VOIKKO_OPT_HYPHENATE_UNKNOWN_WORDS 15
-;; VOIKKO_OPT_ACCEPT_BULLETED_LISTS_IN_GC 16
 
 (defparameter *voikko-boolean-options*
   '((:ignore-dot . 0)
@@ -56,9 +36,6 @@
     (:accept-unfinished-paragraphs-in-gc . 14)
     (:hyphenate-unknown-words . 15)
     (:accept-bulleted-lists-in-gc . 16)))
-
-;; VOIKKO_MIN_HYPHENATED_WORD_LENGTH 9
-;; VOIKKO_SPELLER_CACHE_SIZE 17
 
 (defparameter *voikko-integer-options*
   '((:min-hyphenated-word-length . 9)
