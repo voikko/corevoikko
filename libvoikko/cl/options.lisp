259af8f4786b63a9c89476be201df3e61917f51e
Teemu Likonen <tlikonen@iki.fi>
2012-01-29 16:46:29 +0000

    CL: Update copyright years to 2011-2012
    git-svn-id: https://voikko.svn.sourceforge.net/svnroot/voikko/trunk@4118 ee56993d-3414-0410-8ed4-8136e77a0dd8

--- libvoikko/cl/options.lisp
+++ libvoikko/cl/options.lisp
@@ -1,6 +1,6 @@
 ;;;; A Common Lisp interface for libvoikko
 ;;
-;; Copyright (C) 2011 Teemu Likonen <tlikonen@iki.fi>
+;; Copyright (C) 2011-2012 Teemu Likonen <tlikonen@iki.fi>
 ;;
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
