b582f9a88b0bd53fbbcd10944f442cdec17f64f4
Harri Pitkänen <hatapitk@iki.fi>
2012-12-13 21:47:12 +0200

    Add Apaja

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -235,6 +235,7 @@ def handle_word(word):
 		                 u"autio", \
 		                 u"apila", \
 		                 u"korkea", \
+		                 u"apaja", \
 		                 u"hame", u"enne", u"helle", u"kierre", u"lumme", u"lahje", u"lape", u"sade", u"taive", u"tarvike", u"vaate", \
 		                 u"alaston", u"onneton", \
 		                 u"uistin", u"vaimennin", u"sivellin", u"kiharrin", u"kaadin", u"kaavin", u"suodatin", u"puin", u"poljin", \
