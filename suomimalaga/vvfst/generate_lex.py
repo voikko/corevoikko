979158bf29e3415f9e37a4a704f09ea6be9d0b3b
Harri Pitkänen <hatapitk@iki.fi>
2012-12-14 21:24:17 +0200

    Add Lohi, Tuohi

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -236,6 +236,7 @@ def handle_word(word):
 		                 u"apila", \
 		                 u"korkea", \
 		                 u"apaja", \
+		                 u"lohi", u"tuohi", \
 		                 u"hame", u"enne", u"helle", u"kierre", u"lumme", u"lahje", u"lape", u"sade", u"taive", u"tarvike", u"vaate", \
 		                 u"alaston", u"onneton", \
 		                 u"uistin", u"vaimennin", u"sivellin", u"kiharrin", u"kaadin", u"kaavin", u"suodatin", u"puin", u"poljin", \
