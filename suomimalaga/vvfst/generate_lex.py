b33493a2e1bf149ecff1dd27ec11bcd90033e91c
Harri Pitkänen <hatapitk@iki.fi>
2012-12-28 08:57:22 +0200

    Add Sallia, Onkia

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -252,6 +252,7 @@ def handle_word(word):
 		                 u"koira", u"juhta", u"kenttä", u"honka", u"kompa", u"suunta", u"tolppa", u"turta", u"kulta", u"hoikka", u"huopa", u"ikä", \
 		                 u"tytär", \
 		                 u"aavistaa", u"muistaa", u"kirjoittaa", u"heittää", \
+		                 u"sallia", u"onkia", \
 		                 u"kanavoida", \
 		                 u"hidastaa", \
 		                 u"ammottaa", \
