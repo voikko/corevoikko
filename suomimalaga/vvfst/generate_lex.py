5a6574a31c44e6b3f15aaf1d530cf77858c18cab
Harri Pitkänen <hatapitk@iki.fi>
2013-01-21 21:40:03 +0200

    Add Kuollut, Johdin_nUt

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -250,6 +250,7 @@ def handle_word(word):
 		                 u"susi", \
 		                 u"karahka", \
 		                 u"solakka", u"ulappa", u"savotta", \
+		                 u"kuollut", \
 		                 u"hame", u"enne", u"helle", u"kierre", u"lumme", u"lahje", u"lape", u"sade", u"taive", u"tarvike", u"vaate", \
 		                 u"alaston", u"onneton", \
 		                 u"uistin", u"vaimennin", u"sivellin", u"kiharrin", u"kaadin", u"kaavin", u"suodatin", u"puin", u"poljin", \
