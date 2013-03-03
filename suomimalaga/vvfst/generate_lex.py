c4d3976373453a12cdf335bc09c4c05c3d7b9f3d
Harri Pitkänen <hatapitk@iki.fi>
2013-03-03 10:57:32 +0200

    Add Nuolaista, Rangaista, Kalista, Vavista

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -303,6 +303,7 @@ def handle_word(word):
 		                 u"kaivaa", u"lappaa", u"antaa", u"jakaa", u"raataa", u"saattaa", \
 		                 u"salata", u"kammata", u"hangata", u"kerrata", u"suunnata", u"vallata", u"lobata", u"kelvata", u"kuitata", u"ladata", u"peljätä", u"pakata", u"pompata", u"taata", \
 		                 u"arvailla", u"tulla", u"mennä", \
+		                 u"nuolaista", u"rangaista", u"kalista", u"vavista", \
 		                 u"katsella", u"kohdella", u"nakella", u"tapella", u"aatella", u"askarrella", u"ommella", u"pienennellä", u"takellella"]:
 			continue
 		vfst_class_prefix = get_vfst_class_prefix(vfst_word_class)
