a23d220a485b2a576b2931171024bf5241f055d6
Harri Pitkänen <hatapitk@iki.fi>
2013-03-12 13:39:15 +0200

    Add Hujahtaa

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -300,7 +300,7 @@ def handle_word(word):
 		                 u"huutaa", u"soutaa", u"vuotaa", \
 		                 u"autioittaa", u"sulaa", u"kyntää", \
 		                 u"valita", \
-		                 u"hohtaa", \
+		                 u"hohtaa", u"hujahtaa", \
 		                 u"saada", u"myydä", u"juoda", u"viedä", u"voida", \
 		                 u"laskea", u"hakea", u"polkea", u"tunkea", u"kutea", u"rypeä", \
 		                 u"punoa", u"kaatua", u"tempoa", u"asettua", u"viittoa", u"aikoa", u"kiekua", u"harppoa", u"kieppua", u"jakaantua", u"kertoa", u"kumartua", u"mankua", u"penkoa", u"paleltua", u"tahtoa", u"leipoa", u"saapua", u"liukua", \
