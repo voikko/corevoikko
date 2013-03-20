67f79a1d4b6e0dc9451e3ee3fe2390bf75111f55
Harri Pitkänen <hatapitk@iki.fi>
2013-03-20 17:35:19 +0200

    Add Kamee, Trikoo, Nugaa, Revyy

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -286,6 +286,7 @@ def handle_word(word):
 		                 u"alaston", u"onneton", \
 		                 u"uistin", u"vaimennin", u"sivellin", u"kiharrin", u"kaadin", u"kaavin", u"suodatin", u"puin", u"poljin", \
 		                 u"vastaus", u"kalleus", \
+		                 u"kamee", u"trikoo", u"nugaa", u"revyy", \
 		                 u"iäkäs", u"vilkas", u"avokas", u"ahdas", u"allas", u"hammas", u"kinnas", u"varas", u"saapas", u"ratas", u"varvas", u"kangas", \
 		                 u"risti", u"takki", u"tatti", u"tunti", u"keppi", u"vati", u"pelti", u"renki", u"hupi", \
 		                 u"kalsium", u"nainen", u"vieras", u"asiakas", u"harras", \
