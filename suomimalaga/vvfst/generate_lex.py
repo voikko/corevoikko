aa44dbec86801e5966af32169e15dcb22ef7dd3c
Harri Pitkänen <hatapitk@iki.fi>
2013-03-26 18:12:05 +0200

    Add Aika

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -294,7 +294,7 @@ def handle_word(word):
 		                 u"risti", u"takki", u"tatti", u"tunti", u"keppi", u"vati", u"pelti", u"renki", u"hupi", \
 		                 u"kalsium", u"nainen", u"vieras", u"asiakas", u"harras", \
 		                 u"lovi", u"kaikki", u"hanki", u"happi", u"lampi", u"sampi", u"kylki", u"lahti", u"siipi", u"lehti", \
-		                 u"kala", u"aitta", u"kappa", u"napa", u"kampa", u"hanka", u"kanta", u"parta", u"raaka", u"valta", u"haka", u"pata", \
+		                 u"kala", u"aitta", u"kappa", u"napa", u"kampa", u"hanka", u"kanta", u"parta", u"raaka", u"valta", u"haka", u"pata", u"aika", \
 		                 u"koira", u"juhta", u"kenttä", u"honka", u"kompa", u"suunta", u"tolppa", u"turta", u"kulta", u"hoikka", u"huopa", u"ikä", \
 		                 u"tytär", u"sisar", u"ien", \
 		                 u"aavistaa", u"muistaa", u"kirjoittaa", u"heittää", u"alittaa", u"astahtaa", \
