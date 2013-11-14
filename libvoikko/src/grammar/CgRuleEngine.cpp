250167bb1bbfe67095be2ca6a4be63abd542db7b
Francis Tyers <ftyers@prompsit.com>
2013-11-14 16:11:01 +0000

    suggestions not in legacyError

--- libvoikko/src/grammar/CgRuleEngine.cpp
+++ libvoikko/src/grammar/CgRuleEngine.cpp
@@ -171,7 +171,7 @@ void CgRuleEngine::check(const Paragraph * paragraph) {
 //						const char *lwf = cg3_tag_gettext_u8(lwt);
 						e->error.legacyError.startpos = cur_pos;
 						e->error.legacyError.errorlen = wcslen(cg3_tag_gettext_w(lwt)) - 4;
-						e->error.legacyError.suggestions.push_back(string("beasssat"));
+						e->error.suggestions.push_back(string("beasssat"));
 						options->grammarChecker->cache.appendError(e);
 						fprintf(stderr, "  pos: %ld; len: %ld\n", cur_pos, wcslen(cg3_tag_gettext_w(lwt)) - 4);
 					}
