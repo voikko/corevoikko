04c8e935b0afc6f9c7fa927a8f6ba7728c5aa8bd
Harri Pitkänen <hatapitk@iki.fi>
2014-02-01 13:55:29 +0200

    Add support for ei_mainen

--- suomimalaga/vvfst/generate_lex.py
+++ suomimalaga/vvfst/generate_lex.py
@@ -169,6 +169,8 @@ def get_diacritics(word, altforms, vfst_word_class):
 				diacritics.append(u"@P.INEN_SALLITTU.ON@")
 			elif flagName == u"ei_inen":
 				diacritics.append(u"@P.INEN_KIELLETTY.ON@")
+			elif flagName == u"ei_mainen":
+				diacritics.append(u"@P.EI_MAINEN.ON@")
 			elif flagName == u"ym3":
 				diacritics.append(u"@P.VAIN_YM3.ON@")
 			elif flagName == u"geo_suffix":
