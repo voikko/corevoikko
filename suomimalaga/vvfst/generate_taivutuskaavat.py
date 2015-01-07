658457ef3ecf3e255dfb6f7705ddd10e7737bbe1
Hannu Väisänen <Hannu.Vaisanen@uef.fi>
2015-01-07 08:11:24 +0200

    Add old inflections for Sukija indexer.

--- suomimalaga/vvfst/generate_taivutuskaavat.py
+++ suomimalaga/vvfst/generate_taivutuskaavat.py
@@ -124,7 +124,7 @@ def appendLexicon(lexiconName, lines, lexcFile):
 		                u"OlVLoppu", u"MonikonNominatiivinJatko"]:
 			appendLines(u"LukusananJälkiliite", realName, lines, lexcFile)
 			appendLines(u"Asemosana", realName, lines, lexcFile)
-		if OPTIONS["sukija"] and realName in [u"SukijaYhteisetHMuodot"]:
+		if OPTIONS["sukija"] and realName in [u"SukijaLOiMonikko", u"SukijaYhteisetHMuodot"]:
 			appendLines(u"LukusananJälkiliite", realName, lines, lexcFile)
 			appendLines(u"Asemosana", realName, lines, lexcFile)
 	else:
