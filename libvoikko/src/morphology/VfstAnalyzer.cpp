8882b0190602deec84281541f332231d472e1ca4
Harri Pitkänen <hatapitk@iki.fi>
2015-02-08 20:57:03 +0200

    Remove incorrect attribute

--- libvoikko/src/morphology/VfstAnalyzer.cpp
+++ libvoikko/src/morphology/VfstAnalyzer.cpp
@@ -425,7 +425,7 @@ static void addInfoFlag(Analysis * analysis, const wchar_t * outputPosition, con
 		}
 	}
 	else if (wcsncmp(outputPosition, L"ca", 2) == 0) {
-		if (!wcsstr(outputPosition, L"[Bc]") && (!className || wcsncmp(className, L"nimisana", 8) == 0)) {
+		if (!wcsstr(outputPosition, L"[Bc]") && !wcsstr(outputPosition, L"[Ll]") && (!className || wcsncmp(className, L"nimisana", 8) == 0)) {
 			analysis->addAttribute("POSSIBLE_GEOGRAPHICAL_NAME", StringUtils::copy(L"true"));
 		}
 	}
