8017cc7f406f6424320a907b45e7a56e0647fd25
Harri Pitkänen <hatapitk@iki.fi>
2016-02-25 15:24:00 +0200

    Basic spell checking sort of works

--- libvoikko/js/build.sh
+++ libvoikko/js/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 emmake make
-emcc src/.libs/libvoikko.so.1.14.3 -o js/libvoikko.html -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr']"
+emcc src/.libs/libvoikko.so.1.14.3 --preload-file 5 -o js/libvoikko.html -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr']"
--- libvoikko/js/libvoikko_api.js
+++ libvoikko/js/libvoikko_api.js
@@ -1,1 +1,3 @@
+var c_init = Module.cwrap('voikkoInit', 'number', ['number', 'string', 'string'])
+var c_spellCstr = Module.cwrap('voikkoSpellCstr', 'number', ['number', 'string'])
 var c_freeCstr = Module.cwrap('voikkoFreeCstr', null, ['number']);
