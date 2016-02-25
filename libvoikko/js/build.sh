058d06b909a5fb242ee22dd2345017447e3be9da
Harri Pitkänen <hatapitk@iki.fi>
2016-02-25 16:33:35 +0200

    Modularize the JS code

--- libvoikko/js/build.sh
+++ libvoikko/js/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 emmake make
-emcc src/.libs/libvoikko.so.1.14.3 --preload-file 5 -o js/libvoikko.html -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr']"
+emcc src/.libs/libvoikko.so.1.14.3 --preload-file 5 -o js/libvoikko.html --post-js js/libvoikko_api.js -s MODULARIZE="1" -s EXPORT_NAME="'Libvoikko'" -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr']"
--- libvoikko/js/libvoikko_api.js
+++ libvoikko/js/libvoikko_api.js
@@ -1,3 +1,12 @@
 var c_init = Module.cwrap('voikkoInit', 'number', ['number', 'string', 'string'])
 var c_spellCstr = Module.cwrap('voikkoSpellCstr', 'number', ['number', 'string'])
 var c_freeCstr = Module.cwrap('voikkoFreeCstr', null, ['number']);
+
+Module.init = function(lang, path) {
+	var handle = c_init(0, lang, path);
+	return {
+		spell: function(word) {
+			return c_spellCstr(handle, word);
+		}
+	}
+}
