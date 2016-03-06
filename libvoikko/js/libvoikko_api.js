aa0df506c57714b8402f77e467afbbdbbda3e1ea
Harri Pitkänen <hatapitk@iki.fi>
2016-03-06 22:20:58 +0200

    Enable closure compiler

--- libvoikko/js/libvoikko_api.js
+++ libvoikko/js/libvoikko_api.js
@@ -1,54 +1,57 @@
 var PTR_SIZE = 4;
-var c_init = Module.cwrap('voikkoInit', 'number', ['number', 'string', 'string']);
-var c_terminate = Module.cwrap('voikkoTerminate', null, ['number']);
-var c_setBooleanOption = Module.cwrap('voikkoSetBooleanOption', 'number', ['number', 'number', 'number']);
-var c_setIntegerOption = Module.cwrap('voikkoSetIntegerOption', 'number', ['number', 'number', 'number']);
-var c_spellCstr = Module.cwrap('voikkoSpellCstr', 'number', ['number', 'string']);
-var c_suggestCstr = Module.cwrap('voikkoSuggestCstr', 'number', ['number', 'string']);
-var c_hyphenateCstr = Module.cwrap('voikkoHyphenateCstr', 'number', ['number', 'string']);
-var c_freeCstrArray = Module.cwrap('voikkoFreeCstrArray', null, ['number']);
-var c_freeCstr = Module.cwrap('voikkoFreeCstr', null, ['number']);
-var c_nextTokenCstr = Module.cwrap('voikkoNextTokenCstr', 'number', ['number', 'number', 'number', 'number']);
-var c_nextSentenceStartCstr = Module.cwrap('voikkoNextSentenceStartCstr', 'number', ['number', 'number', 'number', 'number']);
-var c_nextGrammarErrorCstr = Module.cwrap('voikkoNextGrammarErrorCstr', 'number', ['number', 'string', 'number', 'number', 'number']);
-var c_grammarErrorCode = Module.cwrap('voikkoGetGrammarErrorCode', 'number', ['number']);
-var c_grammarErrorStartPos = Module.cwrap('voikkoGetGrammarErrorStartPos', 'number', ['number']);
-var c_grammarErrorLength = Module.cwrap('voikkoGetGrammarErrorLength', 'number', ['number']);
-var c_grammarErrorSuggestions = Module.cwrap('voikkoGetGrammarErrorSuggestions', 'number', ['number']);
-var c_freeGrammarError = Module.cwrap('voikkoFreeGrammarError', null, ['number']);
-var c_grammarErrorShortDescription = Module.cwrap('voikkoGetGrammarErrorShortDescription', 'number', ['number', 'string']);
-var c_freeErrorMessageCstr = Module.cwrap('voikkoFreeErrorMessageCstr', null, ['number']);
-var c_analyzeWordCstr = Module.cwrap('voikkoAnalyzeWordCstr', 'number', ['number', 'string']);
-var c_freeMorAnalysis = Module.cwrap('voikko_free_mor_analysis', null, ['number']);
-var c_morAnalysisKeys = Module.cwrap('voikko_mor_analysis_keys', 'number', ['number']);
-var c_morAnalysisValueCstr = Module.cwrap('voikko_mor_analysis_value_cstr', 'number', ['number', 'number']);
-var c_freeMorAnalysisValueCstr = Module.cwrap('voikko_free_mor_analysis_value_cstr', null, ['number']);
+var c_malloc = Module["_malloc"];
+var c_free = Module["_free"];
+var cwrap = Module["cwrap"];
+var c_init = cwrap('voikkoInit', 'number', ['number', 'string', 'string']);
+var c_terminate = cwrap('voikkoTerminate', null, ['number']);
+var c_setBooleanOption = cwrap('voikkoSetBooleanOption', 'number', ['number', 'number', 'number']);
+var c_setIntegerOption = cwrap('voikkoSetIntegerOption', 'number', ['number', 'number', 'number']);
+var c_spellCstr = cwrap('voikkoSpellCstr', 'number', ['number', 'string']);
+var c_suggestCstr = cwrap('voikkoSuggestCstr', 'number', ['number', 'string']);
+var c_hyphenateCstr = cwrap('voikkoHyphenateCstr', 'number', ['number', 'string']);
+var c_freeCstrArray = cwrap('voikkoFreeCstrArray', null, ['number']);
+var c_freeCstr = cwrap('voikkoFreeCstr', null, ['number']);
+var c_nextTokenCstr = cwrap('voikkoNextTokenCstr', 'number', ['number', 'number', 'number', 'number']);
+var c_nextSentenceStartCstr = cwrap('voikkoNextSentenceStartCstr', 'number', ['number', 'number', 'number', 'number']);
+var c_nextGrammarErrorCstr = cwrap('voikkoNextGrammarErrorCstr', 'number', ['number', 'string', 'number', 'number', 'number']);
+var c_grammarErrorCode = cwrap('voikkoGetGrammarErrorCode', 'number', ['number']);
+var c_grammarErrorStartPos = cwrap('voikkoGetGrammarErrorStartPos', 'number', ['number']);
+var c_grammarErrorLength = cwrap('voikkoGetGrammarErrorLength', 'number', ['number']);
+var c_grammarErrorSuggestions = cwrap('voikkoGetGrammarErrorSuggestions', 'number', ['number']);
+var c_freeGrammarError = cwrap('voikkoFreeGrammarError', null, ['number']);
+var c_grammarErrorShortDescription = cwrap('voikkoGetGrammarErrorShortDescription', 'number', ['number', 'string']);
+var c_freeErrorMessageCstr = cwrap('voikkoFreeErrorMessageCstr', null, ['number']);
+var c_analyzeWordCstr = cwrap('voikkoAnalyzeWordCstr', 'number', ['number', 'string']);
+var c_freeMorAnalysis = cwrap('voikko_free_mor_analysis', null, ['number']);
+var c_morAnalysisKeys = cwrap('voikko_mor_analysis_keys', 'number', ['number']);
+var c_morAnalysisValueCstr = cwrap('voikko_mor_analysis_value_cstr', 'number', ['number', 'number']);
+var c_freeMorAnalysisValueCstr = cwrap('voikko_free_mor_analysis_value_cstr', null, ['number']);
 
 var mallocPtr = function() {
-	return Module._malloc(PTR_SIZE);
+	return c_malloc(PTR_SIZE);
 };
 
 var isValidInput = function(word) {
 	return word.indexOf('\0') == -1;
 }
 
-Module.init = function(lang, path) {
+Module["init"] = function(lang, path) {
 	var errorPtr = mallocPtr();
 	var handle = c_init(errorPtr, lang, path);
 	if (!handle) {
 		var errorStrPtr = getValue(errorPtr, "i32*");
 		var errorStr = UTF8ToString(errorStrPtr);
-		Module._free(errorPtr);
+		c_free(errorPtr);
 		throw errorStr;
 	}
-	Module._free(errorPtr);
+	c_free(errorPtr);
 	
 	var tokenTypes = ["NONE", "WORD", "PUNCTUATION", "WHITESPACE", "UNKNOWN"];
 	var sentenceTypes = ["NONE", "NO_START", "PROBABLE", "POSSIBLE"];
 	var tokensNonNull = function(text) {
 		var result = [];
 		var textLengthUtf8 = lengthBytesUTF8(text);
-		var textBytes = Module._malloc(textLengthUtf8 + 1);
+		var textBytes = c_malloc(textLengthUtf8 + 1);
 		stringToUTF8(text, textBytes, textLengthUtf8 + 1);
 		var bytesStart = 0;
 		var textStart = 0;
@@ -66,8 +69,8 @@ Module.init = function(lang, path) {
 			bytesStart += tokenBytes;
 			bytesLen -= tokenBytes;
 		}
-		Module._free(tokenLenByRef);
-		Module._free(textBytes);
+		c_free(tokenLenByRef);
+		c_free(textBytes);
 		return result;
 	}
 	
@@ -130,11 +133,11 @@ Module.init = function(lang, path) {
 		var shortDescription = UTF8ToString(cShortDescription);
 		c_freeErrorMessageCstr(cShortDescription);
 		return {
-			errorCode: errorCode,
-			startPos: offset + startPos,
-			errorLen: errorLength,
-			suggestions: suggestions,
-			shortDescription: shortDescription
+			"errorCode": errorCode,
+			"startPos": offset + startPos,
+			"errorLen": errorLength,
+			"suggestions": suggestions,
+			"shortDescription": shortDescription
 		};
 	};
 	var appendErrorsFromParagraph = function(errorList, paragraph, offset, language) {
@@ -158,19 +161,19 @@ Module.init = function(lang, path) {
 	};
 	
 	return {
-		terminate: function() {
+		"terminate": function() {
 			if (handle) {
 				c_terminate(handle);
 				handle = null;
 			}
 		},
 		
-		spell: function(word) {
+		"spell": function(word) {
 			requireValidHandle();
 			return isValidInput(word) && c_spellCstr(handle, word) == 1;
 		},
 		
-		suggest: function(word) {
+		"suggest": function(word) {
 			requireValidHandle();
 			var suggestions = [];
 			if (!isValidInput(word)) {
@@ -185,7 +188,7 @@ Module.init = function(lang, path) {
 			return suggestions;
 		},
 		
-		grammarErrors: function(text, language) {
+		"grammarErrors": function(text, language) {
 			requireValidHandle();
 			var errorList = [];
 			if (!isValidInput(text)) {
@@ -201,7 +204,7 @@ Module.init = function(lang, path) {
 			return errorList;
 		},
 		
-		analyze: function(word) {
+		"analyze": function(word) {
 			requireValidHandle();
 			var analysisList = [];
 			if (!isValidInput(word)) {
@@ -234,28 +237,28 @@ Module.init = function(lang, path) {
 			return analysisList;
 		},
 		
-		tokens: function(text) {
+		"tokens": function(text) {
 			requireValidHandle();
 			var allTokens = [];
 			var lastStart = 0;
 			for (var i = text.indexOf("\0"); i != -1; i = text.indexOf("\0", i + 1)) {
 				allTokens.push.apply(allTokens, tokensNonNull(text.substring(lastStart, i)));
-				allTokens.push({type: "UNKNOWN", text: "\0"});
+				allTokens.push({"type": "UNKNOWN", "text": "\0"});
 				lastStart = i + 1;
 			}
 			allTokens.push.apply(allTokens, tokensNonNull(text.substring(lastStart)));
 			return allTokens;
 		},
 		
-		sentences: function(text) {
+		"sentences": function(text) {
 			requireValidHandle();
 			var result = [];
 			if (!isValidInput(text)) {
-				result.push({text: text, nextStartType: "NONE"});
+				result.push({"text": text, "nextStartType": "NONE"});
 				return result;
 			}
 			var textLen = lengthBytesUTF8(text);
-			var textBytes = Module._malloc(textLen + 1);
+			var textBytes = c_malloc(textLen + 1);
 			var textBytesPtr = textBytes;
 			stringToUTF8(text, textBytes, textLen + 1);
 			var sentenceLenByRef = mallocPtr();
@@ -264,20 +267,20 @@ Module.init = function(lang, path) {
 				var sentenceLen = getValue(sentenceLenByRef, "i32");
 				var sentenceType = sentenceTypes[sentenceTypeInt];
 				var sentenceText = text.substring(0, sentenceLen);
-				result.push({text: sentenceText, nextStartType: sentenceType});
+				result.push({"text": sentenceText, "nextStartType": sentenceType});
 				text = text.substring(sentenceLen);
 				var sentenceLenBytes = lengthBytesUTF8(sentenceText);
 				textBytesPtr += sentenceLenBytes;
 				textLen -= sentenceLenBytes;
 			}
-			Module._free(sentenceLenByRef);
-			Module._free(textBytes);
+			c_free(sentenceLenByRef);
+			c_free(textBytes);
 			return result;
 		},
 		
-		getHyphenationPattern: getHyphenationPattern,
+		"getHyphenationPattern": getHyphenationPattern,
 		
-		hyphenate: function(word) {
+		"hyphenate": function(word) {
 			var pattern = getHyphenationPattern(word);
 			var hyphenated = "";
 			for (var i = 0; i < pattern.length; i++) {
@@ -294,63 +297,63 @@ Module.init = function(lang, path) {
 			return hyphenated;
 		},
 		
-		setIgnoreDot: function(value) {
+		"setIgnoreDot": function(value) {
 			setBoolOption(0, value);
 		},
 		
-		setIgnoreNumbers: function(value) {
+		"setIgnoreNumbers": function(value) {
 			setBoolOption(1, value);
 		},
 		
-		setIgnoreUppercase: function(value) {
+		"setIgnoreUppercase": function(value) {
 			setBoolOption(3, value);
 		},
 		
-		setAcceptFirstUppercase: function(value) {
+		"setAcceptFirstUppercase": function(value) {
 			setBoolOption(6, value);
 		},
 		
-		setAcceptAllUppercase: function(value) {
+		"setAcceptAllUppercase": function(value) {
 			setBoolOption(7, value);
 		},
 		
-		setIgnoreNonwords: function(value) {
+		"setIgnoreNonwords": function(value) {
 			setBoolOption(10, value);
 		},
 		
-		setAcceptExtraHyphens: function(value) {
+		"setAcceptExtraHyphens": function(value) {
 			setBoolOption(11, value);
 		},
 		
-		setAcceptMissingHyphens: function(value) {
+		"setAcceptMissingHyphens": function(value) {
 			setBoolOption(12, value);
 		},
 		
-		setAcceptTitlesInGc: function(value) {
+		"setAcceptTitlesInGc": function(value) {
 			setBoolOption(13, value);
 		},
 		
-		setAcceptUnfinishedParagraphsInGc: function(value) {
+		"setAcceptUnfinishedParagraphsInGc": function(value) {
 			setBoolOption(14, value);
 		},
 		
-		setAcceptBulletedListsInGc: function(value) {
+		"setAcceptBulletedListsInGc": function(value) {
 			setBoolOption(16, value);
 		},
 		
-		setNoUglyHyphenation: function(value) {
+		"setNoUglyHyphenation": function(value) {
 			setBoolOption(4, value);
 		},
 		
-		setHyphenateUnknownWords: function(value) {
+		"setHyphenateUnknownWords": function(value) {
 			setBoolOption(15, value);
 		},
 		
-		setMinHyphenatedWordLength: function(value) {
+		"setMinHyphenatedWordLength": function(value) {
 			setIntegerOption(9, value);
 		},
 		
-		setSuggestionStrategy: function(value) {
+		"setSuggestionStrategy": function(value) {
 			if (value == "TYPO") {
 				setBoolOption(8, false);
 			}
