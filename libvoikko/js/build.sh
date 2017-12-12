381093a7aee08f1e493e994a4c1872d3b35e3bbc
Harri Pitkänen <hatapitk@iki.fi>
2017-12-12 16:23:15 +0200

    Add shell script parameters for some basic scenarios

--- libvoikko/js/build.sh
+++ libvoikko/js/build.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
+if [ $# -eq 0 ]
+then
+   echo "Usage: $0 embed | preload | plain"
+   echo ""
+   exit 0
+fi
+
+OPTS="--closure 1"
+
+case $1 in
+   embed)
+     # Closure compiler may run out of memory when large dictionary files are embedded.
+     OPTS="--closure 0 --embed-file 5"
+     ;;
+   preload)
+     OPTS="--closure 1 --preload-file 5"
+     ;;
+esac
+
 emmake make
 
-# The following command uses --preload-file which will only work in browser environment. Use --embed-file instead for Node.
-# Also note that closure compiler may run out of memory when large dictionary files are embedded.
-emcc -g0 -O3 src/.libs/libvoikko.so.1.14.5 --memory-init-file 0 --closure 1 --preload-file 5 -o js/libvoikko.js --post-js js/libvoikko_api.js -s MODULARIZE="1" -s EXPORT_NAME="'Libvoikko'" -s NO_EXIT_RUNTIME="1" -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr','_voikkoNextTokenCstr','_voikkoNextSentenceStartCstr','_voikkoNextGrammarErrorCstr','_voikkoGetGrammarErrorCode','_voikkoGetGrammarErrorStartPos','_voikkoGetGrammarErrorLength','_voikkoGetGrammarErrorSuggestions','_voikkoFreeGrammarError','_voikkoGetGrammarErrorShortDescription','_voikkoFreeErrorMessageCstr','_voikko_list_dicts','_voikko_free_dicts','_voikko_dict_language','_voikko_dict_script','_voikko_dict_variant','_voikko_dict_description','_voikkoGetVersion','_voikkoAnalyzeWordCstr','_voikko_free_mor_analysis','_voikko_mor_analysis_keys','_voikko_mor_analysis_value_cstr','_voikko_free_mor_analysis_value_cstr']"
+emcc -g0 -O3 src/.libs/libvoikko.so.1.14.5 --memory-init-file 0 $OPTS -o js/libvoikko.js --post-js js/libvoikko_api.js -s MODULARIZE="1" -s EXPORT_NAME="'Libvoikko'" -s NO_EXIT_RUNTIME="1" -s EXPORTED_FUNCTIONS="['_voikkoInit','_voikkoTerminate','_voikkoSetBooleanOption','_voikkoSetIntegerOption','_voikkoSpellCstr','_voikkoSuggestCstr','_voikkoHyphenateCstr','_voikkoFreeCstrArray','_voikkoFreeCstr','_voikkoNextTokenCstr','_voikkoNextSentenceStartCstr','_voikkoNextGrammarErrorCstr','_voikkoGetGrammarErrorCode','_voikkoGetGrammarErrorStartPos','_voikkoGetGrammarErrorLength','_voikkoGetGrammarErrorSuggestions','_voikkoFreeGrammarError','_voikkoGetGrammarErrorShortDescription','_voikkoFreeErrorMessageCstr','_voikko_list_dicts','_voikko_free_dicts','_voikko_dict_language','_voikko_dict_script','_voikko_dict_variant','_voikko_dict_description','_voikkoGetVersion','_voikkoAnalyzeWordCstr','_voikko_free_mor_analysis','_voikko_mor_analysis_keys','_voikko_mor_analysis_value_cstr','_voikko_free_mor_analysis_value_cstr']"
 cat js/libvoikko.js js/commonjs-footer.js > js/js-libvoikko.js
