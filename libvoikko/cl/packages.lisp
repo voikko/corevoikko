1de75de7d4b28a5bdaceca39fc12a63d8ad4a8d3
Teemu Likonen <tlikonen@iki.fi>
2019-07-10 21:50:21 +0300

    CL: Export symbol INSERT-HYPHENS from VOIKKO package

--- libvoikko/cl/packages.lisp
+++ libvoikko/cl/packages.lisp
@@ -4,6 +4,7 @@
   (:use #:cl #:cffi)
   (:export #:instance #:activep #:initialize #:terminate
            #:with-instance #:spell #:suggest #:hyphenate #:version
+           #:insert-hyphens
            #:set-option #:analyze #:split-word #:list-dictionaries
            #:list-supported-spelling-languages
            #:list-supported-hyphenation-languages
