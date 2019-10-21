f93f0a2930ad290011da099b7f3438cb26f7a2a0
Niilo Ursin <niilo.ursin@gmail.com>
2019-10-21 23:31:49 +0300

    Add missing Analysis attributes, make all optional and add documentation
    documentation is based on https://github.com/voikko/corevoikko/blob/master/libvoikko/doc/morphological-analysis.txt

--- libvoikko/typescript/libvoikko.d.ts
+++ libvoikko/typescript/libvoikko.d.ts
@@ -14,13 +14,233 @@ declare interface GrammarError {
 }
 
 declare interface Analysis {
-  BASEFORM: string;
-  CLASS: string;
-  FSTOUTPUT: string;
-  NUMBER: string;
-  SIJAMUOTO: string;
-  STRUCTURE: string;
-  WORDBASES: string;
+  /**
+   * Base form of the given word.
+   *
+   * Examples:
+   *   Word: kissalla -> BASEFORM: kissa
+   */
+  BASEFORM?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Sanan sanaluokka. Attribuutti on käytössä libvoikon sisällä.
+   * Attribuutin mahdolliset arvot ovat seuraavat:
+   *  - nimisana (yleisnimi)
+   *  - laatusana
+   *  - nimisana_laatusana (sama kuin erilliset analyysit nimisanana ja laatusanana)
+   *  - teonsana
+   *  - seikkasana
+   *  - asemosana
+   *  - suhdesana
+   *  - huudahdussana
+   *  - sidesana
+   *  - etunimi
+   *  - sukunimi
+   *  - paikannimi
+   *  - nimi (muu erisnimi kuin etu-, suku- tai paikannimi)
+   *  - kieltosana
+   *  - lyhenne
+   *  - lukusana
+   *  - etuliite
+   */
+  CLASS?: string;
+  /**
+   * Word is comparable (adjective). Suggested values for this attribute are
+   * "positive", "comparative" and "superlative".
+   *
+   * Examples:
+   *   Word: sininen   -> COMPARISON: positive
+   *   Word: sinisempi -> COMPARISON: comparative
+   *   Word: sinisin   -> COMPARISON: superlative
+   */
+  COMPARISON?: string;
+  /**
+   * Analyzers that are implemented using finite state transducers can provide
+   * the raw transducer output using this attribute.
+   *
+   * Examples:
+   *   Word: kissalla -> FSTOUTPUT: [Ln][Xp]kissa[X][Xs]505527[X]kissa[Sade][Ny]lla
+   */
+  FSTOUTPUT?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Sanaan liittyy fokuspartikkeli -kin tai -kAAn.
+   *
+   * Esimerkkejä:
+   *   Sana: kissakin  -> FOCUS: kin
+   *   Sana: kissakaan -> FOCUS: kaan
+   */
+  FOCUS?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Sanaan liittyy kysymysliite -ko tai -kö. Attribuutin ainoa sallittu
+   * arvo on "true". Jos sanaan ei liity kysymysliitettä, attribuuttia ei ole.
+   */
+  KYSYMYSLIITE?: string;
+  /**
+   * Mood of a verb. Suggested values for this attribute are
+   * "indicative", "conditional", "imperative" and "potential".
+   *
+   * Examples:
+   *   Word: juoksen -> MOOD: indicative
+   *   Word: juoksisin -> MOOD: conditional
+   */
+  MOOD?: string;
+  /**
+   * For all verbs this attribute indicates whether the verb is in
+   * a connegative form. Suggested values: "false", "true", "both"
+   *
+   * Examples:
+   *   Word: sallitaan -> NEGATIVE: false
+   *   Word: sallita (as in "ei sallita") -> NEGATIVE: true
+   *   Word: maalaa (also "ei maalaa") -> NEGATIVE: both
+   */
+  NEGATIVE?: string;
+  /**
+   * Grammatical number of the word. Suggested values for this attribute
+   * are "singular", "dual", "trial" and "plural".
+   *
+   * Examples:
+   *   Word: kissa -> NUMBER: singular
+   *   Word: kissat -> NUMBER: plural
+   */
+  NUMBER?: string;
+  /**
+   * Word is a participle of some sort. Suggested values for this attribute
+   * are "present_active", "present_passive", "past_active", "past_passive",
+   * "agent" and "negation" (add more as needed).
+   *
+   * Examples:
+   *   Word: juokseva    -> PARTICIPLE: present_active
+   *   Word: juostava    -> PARTICIPLE: present_passive
+   *   Word: juossut     -> PARTICIPLE: past_active
+   *   Word: juostu      -> PARTICIPLE: past_passive
+   *   Word: juoksema    -> PARTICIPLE: agent
+   *   Word: juoksematon -> PARTICIPLE: negation
+   */
+  PARTICIPLE?: string;
+  /**
+   * For verbs in active voice this attribute represents the person
+   * (first, second or third). The person for passive voice can be
+   * considered as the fourth voice if appropriate for the language.
+   * Suggested values for this attribute are "1", "2", "3" and "4".
+   *
+   * Examples:
+   *   Word: juoksen -> PERSON: 1
+   *   Word: juokset -> PERSON: 2
+   */
+  PERSON?: string;
+  /**
+   * Word contains information about possessor. For now this is used to
+   * indicate the use of possessive suffix in Finnish nouns.
+   *
+   * Examples:
+   *   Word: kissani  -> POSSESSIVE: 1s
+   *   Word: kissasi  -> POSSESSIVE: 2s
+   *   Word: kissamme -> POSSESSIVE: 1p
+   *   Word: kissanne -> POSSESSIVE: 2p
+   *   Word: kissansa -> POSSESSIVE: 3
+   */
+  POSSESSIVE?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Nominin sijamuoto. Attribuutti on käytössä libvoikon sisällä.
+   * Attribuutin mahdolliset arvot ovat seuraavat:
+   *  - nimento
+   *  - omanto
+   *  - osanto
+   *  - olento
+   *  - tulento
+   *  - kohdanto
+   *  - sisaolento
+   *  - sisaeronto
+   *  - sisatulento
+   *  - ulkoolento
+   *  - ulkoeronto
+   *  - ulkotulento
+   *  - vajanto
+   *  - seuranto
+   *  - keinonto
+   *  - kerrontosti (esim. "nopeasti")
+   */
+  SIJAMUOTO?: string;
+  /**
+   * This attribute describes morpheme boundaries, character case and
+   * hyphenation restrictions for the word. The following characters are
+   * used in the values of this attribute:
+   *
+   * = Start of a new morpheme. This must also be present at the start
+   *   of a word.
+   *
+   * - Hyphen. Word can be split in text processors after this character
+   *   without inserting an extra hyphen. If the hyphen is at morpheme
+   *   boundary, the boundary symbol = must be placed after the hyphen.
+   *
+   * p Letter that is written in lower case in the standard form.
+   *
+   * q Letter that is written in lower case in the standard form.
+   *   Hyphenation is forbidden before this letter.
+   *
+   * i Letter that is written in upper case in the standard form.
+   *
+   * j Letter that is written in upper case in the standard form.
+   *   Hyphenation is forbidden before this letter.
+   *
+   * Examples:
+   *   Word: Matti-niminen -> STRUCTURE: =ipppp-=ppppppp
+   *   Word: DNA-näyte ->     STRUCTURE: =jjj-=ppppp
+   *   Word: autokauppa ->    STRUCTURE: =pppp=pppppp
+   */
+  STRUCTURE?: string;
+  /**
+   * Tense and aspect of a verb. Suggested values for this attribute are
+   * "past_imperfective", "present_simple", (add more as needed).
+   *
+   * Examples:
+   *   Word: juoksen -> TENSE: present_simple
+   *   Word: juoksin -> TENSE: past_imperfective
+   */
+  TENSE?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Sanan osien perusmuodot. Attribuutti ei ole käytössä libvoikon
+   * sisällä. Attribuutin arvona on sanan perusmuoto, jossa yhdyssanan
+   * osat ja päätteet on erotettu toisistaan +-merkillä. Lisäksi kunkin
+   * yhdyssanan osan perusmuoto on osan perässä suluissa. Mikäli yhdyssanan
+   * osat itsessään ovat jaettavissa osiin, osat voidaan sulkujen sisällä
+   * olevassa perusmuodossa erotella merkeillä = tai |.
+   *
+   * Esimerkkejä:
+   *   Sana: köydenvetoa ->     WORDBASES: +köyde(köysi)+n+veto(veto)
+   *   Sana: Alkio-opistossa -> WORDBASES: +alkio(Alkio)+-+opisto(opisto)
+   *                                       +alkio(alkio)+-+opisto(opisto)
+   *
+   * Johdinpäätteiden perusmuodot ovat suluissa siten, että päätteen edessä
+   * on +-merkki:
+   *   Sana: kansalliseepos ->  WORDBASES: +kansa(kansa)+llis(+llinen)+eepos(eepos)
+   */
+  WORDBASES?: string;
+  /**
+   * Finnish language specific attribute.
+   *
+   * Viittaukset sanan osiin Joukahaisessa. Attribuutti ei ole käytössä
+   * libvoikon sisällä. Attribuutin arvona on sanan perusmuoto, jossa
+   * yhdyssanan osat ja päätteet on erotettu toisistaan +-merkillä.
+   * Lisäksi kunkin yhdyssanan osan tietue-id on osan perässä suluissa.
+   *
+   * Esimerkkejä:
+   *   Sana: köydenvetoa ->     WORDBASES: +köyde(w506953)+n+veto(w517284)
+   *                                       +köyde(w506953)+n+veto(w523540)
+   *                                       +köyde(w506953)+n+veto(w525160)
+   *   Sana: Alkio-opistossa -> WORDBASES: +alkio(w518215)+-+opisto(w510148)
+   *                                       +alkio(w500068)+-+opisto(w510148)
+   */
+  WORDIDS?: string;
 }
 
 declare interface Token {
@@ -92,7 +312,11 @@ declare interface Voikko {
   /**
    * Returns the given word in fully hyphenated form.
    */
-  hyphenate: (word: string, separator?: string, allowContextChanges?: boolean) => string;
+  hyphenate: (
+    word: string,
+    separator?: string,
+    allowContextChanges?: boolean,
+  ) => string;
 
   /**
    * Ignore dot at the end of the word (needed for use in some word processors).
