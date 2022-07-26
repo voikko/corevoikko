e49159f824e80a6dd7925b1fc497b92c90d12a4d
Harri Pitkänen <hatapitk@iki.fi>
2022-07-26 15:23:43 +0300

    Add Java and C# to codeql build

--- codeql-buildscript.sh
+++ codeql-buildscript.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext
 cd libvoikko && ./autogen.sh && ./configure && make
+cd java && mvn compile
+cd ../cs && xbuild libvoikko.sln
