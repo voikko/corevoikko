41be712e47f5d8eb5939873177d7851e4e76837a
Harri Pitkänen <hatapitk@iki.fi>
2022-07-26 15:52:59 +0300

    NUnit is needed for C# build

--- codeql-buildscript.sh
+++ codeql-buildscript.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext
+sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext libnunit-cil-dev
 cd libvoikko && ./autogen.sh && ./configure && make
 cd java && mvn compile
 cd ../cs && xbuild libvoikko.sln
