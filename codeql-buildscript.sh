0f18e2a94a8716055c3d35bf65b187819360a4f9
Harri Pitkänen <hatapitk@iki.fi>
2025-03-29 16:55:50 +0200

    Disable C# in codeql

--- codeql-buildscript.sh
+++ codeql-buildscript.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext libnunit-cil-dev
+sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext mono-xbuild libnunit-cil-dev
 cd libvoikko && ./autogen.sh && ./configure && make
 cd java && mvn compile
-cd ../cs && msbuild libvoikko.sln
+# C# is disabled for now, nunit no longer in debian
+# cd ../cs && xbuild libvoikko.sln
