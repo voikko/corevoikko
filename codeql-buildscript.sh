c9b2faf1689be8d506ded4c1e67362226477a36b
Harri Pitkänen <hatapitk@iki.fi>
2025-03-29 16:38:22 +0200

    Try xbuild -> msbuild

--- codeql-buildscript.sh
+++ codeql-buildscript.sh
@@ -2,4 +2,4 @@
 sudo apt-get install hfst-ospell-dev libtool automake pkg-config gettext libnunit-cil-dev
 cd libvoikko && ./autogen.sh && ./configure && make
 cd java && mvn compile
-cd ../cs && xbuild libvoikko.sln
+cd ../cs && msbuild libvoikko.sln
